bdb13a8ee84b9d525c12188a7a0535e3491d2990
English <englishsnap01@gmail.com>
2026-09-12 16:11:44 -0400

    Add homepage SEO metadata

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,12 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 import "./index.css";
 import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet-async";
+
 import CallToAction from "../../components/CallToAction";
 import Hero from "../../components/Hero";
 import HeroIntroSection from "../../components/HeroIntroSection";
@@ -9,6 +11,13 @@ import WhyChooseUs from "../../components/WhyChooseUs";
 function Home(){
     return(
         <>
+            <Helmet>
+                <title>Techlogex Solutions | Branding, Design & Digital Solutions</title>
+                <meta
+                    name="description"
+                    content="Techlogex Solutions helps businesses build memorable brands through creative design, professional printing, branding, and modern digital solutions."
+/>
+            </Helmet>
             <Hero />
             <HeroIntroSection />
             <HeroServicesPreview />
