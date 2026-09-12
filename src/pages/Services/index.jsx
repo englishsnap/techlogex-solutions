2b58e09e2e0f474ca5c2876b7b435c7a2eb154fe
English <englishsnap01@gmail.com>
2026-09-12 16:23:57 -0400

    Add Services page SEO metadata

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -1,13 +1,22 @@
+import { Helmet } from "react-helmet-async";
+
 import ServicesHero from "../../components/ServicesHero";
 import ServicesList from "../../components/ServicesList";
 
-function services(){
+function Services(){
     return(
         <>
+        <Helmet>
+                <title>Services | Techlogex Solutions</title>
+                <meta
+                    name="description"
+                    content="Explore Techlogex Solutions' creative design, professional printing, branding, and digital services designed to help businesses build strong brands."
+                />
+        </Helmet>
             <ServicesHero />
             <ServicesList />
         </>
     )
 }
 
-export default services;
+export default Services;
